c6ab3c8a238989114ca7979fa8856f5634c45ca3
kenji sakai <kenjisakai80@gmail.com>
2023-03-12 00:10:28 -0300

    1.4 Utilizando o ESLint

--- test/calcula-valor.test.js
+++ test/calcula-valor.test.js
@@ -1,9 +1,26 @@
-const calculaValor = require('../src/calcula-valor.js');
+const calculaValor = require('../src/calcula-valor.js')
 
-test('Com um prestação o montante é igual ao capital', () => {
-  // Operação
-  const montante = calculaValor.calcularMontante(100, 0.0175, 1);
+describe('calcularMontante', () => {
+  test('Com um prestação o montante é igual ao capital', () => {
+    // Operação
+    const montante = calculaValor.calcularMontante(100, 0.0175, 1)
 
-  // Resultado ou Comportamento esperado
-  expect(montante).toBe(100);
-});
+    // Resultado ou Comportamento esperado
+    expect(montante).toBe(100)
+  })
+
+  test('Com 4 prestações o montante é acrescido de juros', () => {
+    // Operação
+    const montante = calculaValor.calcularMontante(500, 0.025, 4)
+
+    // Resultado ou Comportamento esperado
+    expect(montante).toBe(538.45)
+  })
+})
+
+describe('Arredondar', () => {
+  test('Arredondar em duas casas decimais ', () => {
+    const resultado = calculaValor.arredondar(538.4453124999998)
+    expect(resultado).toBe(538.45)
+  })
+})
