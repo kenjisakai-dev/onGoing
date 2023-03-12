f03e7b305e9a8e45e6865c5463c831fa34ed2b61
kenji sakai <kenjisakai80@gmail.com>
2023-03-12 15:00:16 -0300

    1.6. Number.EPSILON – Resposta ao Desafio 1.1

--- test/calcula-valor.test.js
+++ test/calcula-valor.test.js
@@ -1,31 +1,31 @@
-const calculaValor = require('../src/calcula-valor.js');
+const calculaValor = require('../src/calcula-valor.js')
 
 describe('calcularMontante', () => {
   test('Com um prestação o montante é igual ao capital', () => {
     // Operação
-    const montante = calculaValor.calcularMontante(100, 0.0175, 1);
+    const montante = calculaValor.calcularMontante(100, 0.0175, 1)
 
     // Resultado ou Comportamento esperado
-    expect(montante).toBe(100);
-  });
+    expect(montante).toBe(100)
+  })
 
   test('Com 4 prestações o montante é acrescido de juros', () => {
     // Operação
-    const montante = calculaValor.calcularMontante(500, 0.025, 4);
+    const montante = calculaValor.calcularMontante(500, 0.025, 4)
 
     // Resultado ou Comportamento esperado
-    expect(montante).toBe(538.45);
-  });
-});
+    expect(montante).toBe(538.45)
+  })
+})
 
 describe('Arredondar', () => {
   test('Arredondar em duas casas decimais ', () => {
-    const resultado = calculaValor.arredondar(538.4453124999998);
-    expect(resultado).toBe(538.45);
-  });
+    const resultado = calculaValor.arredondar(538.4453124999998)
+    expect(resultado).toBe(538.45)
+  })
 
   test('1.005 deve retornar 1.01', () => {
-    const resultado = calculaValor.arredondar(1.005);
-    expect(resultado).toBe(1.01);
-  });
-});
+    const resultado = calculaValor.arredondar(1.005)
+    expect(resultado).toBe(1.01)
+  })
+})
