cb3d989e13d8cf9a76e879de0c4e3e645de2cc7a
kenji sakai <kenjisakai80@gmail.com>
2023-03-12 14:59:12 -0300

    1.5 Corrigir a falha relatada – Desafio 1.1

--- src/calcula-valor.js
+++ src/calcula-valor.js
@@ -1,16 +1,16 @@
-function calcularMontante (capital, taxa, periodo) {
-  let montante = capital * Math.pow(1 + taxa, periodo - 1)
-  montante = arredondar(montante)
-  return montante
+function calcularMontante(capital, taxa, periodo) {
+  let montante = capital * Math.pow(1 + taxa, periodo - 1);
+  montante = arredondar(montante);
+  return montante;
 }
 
-function arredondar (valor) {
-  const precisao = 100
-  const arredondado = Math.round(valor * precisao) / precisao
-  return arredondado
+function arredondar(valor) {
+  const precisao = 100;
+  const arredondado = Math.round(valor * precisao) / precisao;
+  return arredondado;
 }
 
 module.exports = {
   calcularMontante,
-  arredondar
-}
+  arredondar,
+};
